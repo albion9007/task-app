51cdc76bfc4d616e0004a7e242f8ac8fe715b83a
albion9007 <68537855+albion9007@users.noreply.github.com>
2021-07-01 18:11:37 +0900

    Authenticatorによるsign-up,sign-out機能追加

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import Amplify from "aws-amplify";
+import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import awsConfig from "./aws-exports";
+
+Amplify.configure(awsConfig);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AmplifyAuthenticator>
+      <div className="App">
+        <h1>Welcome to Task App</h1>
+        <AmplifySignOut />
+      </div>
+    </AmplifyAuthenticator>
   );
 }
 
