f81461f112d3105b5caeec19df9d21a92baed6ff
albion9007 <68537855+albion9007@users.noreply.github.com>
2021-08-04 14:20:22 +0900

    Load image from S3

--- src/components/List/Task.js
+++ src/components/List/Task.js
@@ -1,4 +1,5 @@
-import React from "react";
+import { Storage } from "aws-amplify";
+import React, { useEffect, useState } from "react";
 import {
   Container,
   Segment,
@@ -10,32 +11,47 @@ import {
 } from "semantic-ui-react";
 
 export default function Task(props) {
-  const { id, title, description, dispatch } = props;
-  console.log(props)
+  const { id, title, description, imageKey, dispatch } = props;
+  const [imageUrl, setImageUrl] = useState(
+    "https://react.semantic-ui.com/images/wireframe/image.png"
+  );
+
+  const fetchImageUrl = async () => {
+    const imageUrl = await Storage.get(imageKey);
+    setImageUrl(imageUrl);
+  };
+
+  useEffect(() => {
+    if (imageKey) {
+      fetchImageUrl();
+    }
+  }, []);
 
   return (
     <Container attached>
-          <Segment>
-            <Grid columns={2} divided>
-              <Grid.Column stretched width={4}>
-                <Image
-                  size="large"
-                  src="https://react.semantic-ui.com/images/wireframe/image.png"
-                />
-              </Grid.Column>
-              <Grid.Column>
-                <ListHeader as="h1">{title}</ListHeader>
-                <ListDescription>{description}
-                <Icon
-                  name="edit"
-                  className="ml-3"
-                  onClick={() => dispatch({ type: "EDIT_TASK", value: props })}
-                />
-                <Icon name="trash" className="ml-3" onClick={() => dispatch({ type: "DELETE_TASK", value: id })}/>
-                </ListDescription>
-              </Grid.Column>
-            </Grid>
-          </Segment>
+      <Segment>
+        <Grid columns={2} divided>
+          <Grid.Column stretched width={4}>
+            <Image size="large" src={imageUrl} />
+          </Grid.Column>
+          <Grid.Column>
+            <ListHeader as="h1">{title}</ListHeader>
+            <ListDescription>
+              {description}
+              <Icon
+                name="edit"
+                className="ml-3"
+                onClick={() => dispatch({ type: "EDIT_TASK", value: props })}
+              />
+              <Icon
+                name="trash"
+                className="ml-3"
+                onClick={() => dispatch({ type: "DELETE_TASK", value: id })}
+              />
+            </ListDescription>
+          </Grid.Column>
+        </Grid>
+      </Segment>
     </Container>
   );
 }
